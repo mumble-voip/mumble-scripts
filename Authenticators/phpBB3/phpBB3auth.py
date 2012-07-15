18920d639ffbc5a46229a80e98f0a7927b6ccab4
Michael Ziegler <diese-addy@funzt-halt.net>
2012-07-15 11:32:00 +0200

    add a fallback for when Ice.getSliceDir() is empty

--- Authenticators/phpBB3/phpBB3auth.py
+++ Authenticators/phpBB3/phpBB3auth.py
@@ -222,7 +222,10 @@ def do_main_program():
     #--- Authenticator implementation
     #    All of this has to go in here so we can correctly daemonize the tool
     #    without loosing the file descriptors opened by the Ice module
-    Ice.loadSlice('', ['-I' + Ice.getSliceDir(), cfg.ice.slice])
+    slicedir = Ice.getSliceDir()
+    if not slicedir:
+        slicedir = "/usr/share/slice"
+    Ice.loadSlice('', ['-I' + slicedir, cfg.ice.slice])
     import Murmur
     
     class phpBBauthenticatorApp(Ice.Application):
